62522c5852fdb1f07cebb44d1c5a724e0f2a5032
olivermalabar13 <revmalabar13@gmail.com>
2026-01-11 09:17:18 +0800

    .
    .

---
+++ EXER3-C_Malabar/Square.java
@@ -0,0 +1,13 @@
+public class Square extends Shape {
+
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+}
